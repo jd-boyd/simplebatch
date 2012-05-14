69326ba0348d4c0e0b17733428d599cbc0e4329e
Joshua D. Boyd <jdboyd@jdboyd.net>
2012-05-13 21:26:34 -0400

    Change requirements again.  Start building on webob and routes.

--- pbatch/daemons/server.py
+++ pbatch/daemons/server.py
@@ -1,7 +1,11 @@
 import argparse
 import os
 
-import webapp2
+#import webapp2
+import webob
+from webob.dec import wsgify
+
+import routes
 
 import pbatch.model
 
@@ -15,9 +19,12 @@ import pbatch.model
 #     def get(self, id):
 #         print "GET JOB:", id
 
-class next_job(webapp2.RequestHandler):
-    def get(self):
-        self.response.write("NEXT JOB")
+#class next_job(webapp2.RequestHandler):
+#    def get(self):
+class Jobs(object):
+    @wsgify
+    def next_job(self, req):
+        return webob.Response("NEXT JOB")
         #Get the next job
 
 # class run_job(webapp2.RequestHandler):
@@ -30,14 +37,39 @@ class next_job(webapp2.RequestHandler):
 #         self.response.write("COMP JOB")
 #         #get the next job
 
-urls = [
-    ('/jobs/next', next_job),
+#urls = [
+#    ('/jobs/next', next_job),
 #    ('/jobs/run', run_job),
 #    ('/jobs/complete', complete_job),
 #    ('/jobs/([0-9]*)', job),
-]
-app = webapp2.WSGIApplication(urls, debug=True)
+#]
+#app = webapp2.WSGIApplication(urls, debug=True)
+
+map = routes.Mapper()
+map.connect('/jobs/next', view='jobs', method='next_job')
+#map.connect('/view/{item}', view=view)
+
+views = {"jobs": Jobs()}
+
+class Dispatcher(object):
+    @wsgify
+    def __call__(self, req):
+        results = map.routematch(environ=req.environ)
+        if not results:
+            return webob.exc.HTTPNotFound()
+        match, route = results
+        req.urlvars = ((), match)
+        kwargs = match.copy()
+        view = kwargs.pop('view')
+        method = kwargs.pop('method')
+
+        return getattr(views[view], method)(req, **kwargs)
 
 def start():
     print "PB server."
-    app.run()
+    from wsgiref.util import setup_testing_defaults
+    from wsgiref.simple_server import make_server
+
+    httpd = make_server('', 8000, Dispatcher())
+    print "Serving on port 8000..."
+    httpd.serve_forever()
