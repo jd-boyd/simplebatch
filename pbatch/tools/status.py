45d072a121cae7e8474321f50eefd16a326210c4
Joshua D. Boyd <jdboyd@jdboyd.net>
2012-05-29 23:22:28 -0400

    More work on pstatus cli options.

--- pbatch/tools/status.py
+++ pbatch/tools/status.py
@@ -4,18 +4,25 @@ import sys
 import pbatch.client
 
 def status_main(args):
-    parser = argparse.ArgumentParser(description='Submit a job to be run later')
+    parser = argparse.ArgumentParser(description='Show the status of jobs in the system.')
 
     parser.add_argument('--running', action="store_true", default=False)
     parser.add_argument('--pending', action="store_true", default = False)
     parser.add_argument('--complete', action="store_true", default =False)
     parser.add_argument('--killed', action="store_true", default =False)
 
-    parser.add_argument('ARGS', nargs="+")
-    
     options = parser.parse_args(args)
 
+    # If all are false, set all to true so that all are done.
+    o = filter(None, options.__dict__.values())
+    if o==[]:
+        for k in options.__dict__:
+            options.__dict__[k] = True
+
     print options
+
+    if options.complete:
+        pass
 
 def status():
     status_main(sys.argv[1:])
