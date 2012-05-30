460d970f38b12a5a1a22d10720d364280848a504
Joshua D. Boyd <jdboyd@jdboyd.net>
2012-05-30 00:09:43 -0400

    Status tools now supports filtering.

--- pbatch/tools/status.py
+++ pbatch/tools/status.py
@@ -10,19 +10,39 @@ def status_main(args):
     parser.add_argument('--pending', action="store_true", default = False)
     parser.add_argument('--complete', action="store_true", default =False)
     parser.add_argument('--killed', action="store_true", default =False)
+    parser.add_argument('--all', action="store_true", default =False)
 
     options = parser.parse_args(args)
 
     # If all are false, set all to true so that all are done.
     o = filter(None, options.__dict__.values())
     if o==[]:
-        for k in options.__dict__:
-            options.__dict__[k] = True
+        options.__dict__['all'] = True
 
     print options
 
-    if options.complete:
-        pass
+    all_jobs = pbatch.client.get_all_jobs()
+
+    def filt(job):
+        if options.all:
+            return True
+        if options.running and job['status']=="running":
+            return True
+        if options.complete and job['status']=="complete":
+            return True
+        if options.pending and job['status']=="pending":
+            return True
+        if options.killed and job['status']=="killed":
+            return True
+        return False
+
+        
+    jobs_to_print = filter(filt, all_jobs)
+    #TODO Filtering based on flags stuff.
+    
+    for job in jobs_to_print:
+        print "ID: %4d" % job['job_id'], "Status: %9s" % job['status'], "CMD:", job['command']
+    
 
 def status():
     status_main(sys.argv[1:])
