5bbc1b40de32931d42fb53003fac28c94f98dee6
Joshua D. Boyd <jdboyd@jdboyd.net>
2012-05-29 21:21:42 -0400

    Start working on the CLI args for the pstatus tool.

--- pbatch/tools/status.py
+++ pbatch/tools/status.py
@@ -3,8 +3,23 @@ import sys
 
 import pbatch.client
 
+def status_main(args):
+    parser = argparse.ArgumentParser(description='Submit a job to be run later')
+
+    parser.add_argument('--running', action="store_true", default=False)
+    parser.add_argument('--pending', action="store_true", default = False)
+    parser.add_argument('--complete', action="store_true", default =False)
+    parser.add_argument('--killed', action="store_true", default =False)
+
+    parser.add_argument('ARGS', nargs="+")
+    
+    options = parser.parse_args(args)
+
+    print options
+
 def status():
-    pass
+    status_main(sys.argv[1:])
 
 def kill():
+    #TODO: switch to proper argparse
     pbatch.client.kill_job(int(sys.argv[1]))
