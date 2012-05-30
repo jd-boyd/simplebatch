051cf542a3208384663c8c81fb8838321b3e5d05
Joshua D. Boyd <jdboyd@jdboyd.net>
2012-05-29 21:19:32 -0400

    Skip tests to be written.

---
+++ pbatch/tests/test_status.py
@@ -0,0 +1,9 @@
+
+from nose.plugins.skip import Skip, SkipTest
+
+from pbatch.tools import status
+
+def test_status_main():
+    raise SkipTest('Not implemented yet.')
+#status_main(args):
+
---
+++ pbatch/tests/test_sub.py
@@ -0,0 +1,8 @@
+
+from nose.plugins.skip import Skip, SkipTest
+
+from pbatch.tools import sub
+
+def test_submit_main():
+    raise SkipTest('Not implemented yet.')
+#submit_main(args):
