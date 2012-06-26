b16bd8a789847b6721e8afd59de80cbc09e47fb0
Joshua D. Boyd <jdboyd@jdboyd.net>
2012-06-25 23:14:55 -0400

    Manager has basic functionality for 2 works and hard coded path.  Logging and config need help desperately.

--- pbatch/daemons/manager.py
+++ pbatch/daemons/manager.py
@@ -1,5 +1,23 @@
 import os, sys
 import argparse
+import logging
+import time
+
+log = logging.getLogger(__name__)
+
+WORKER_CNT = 2
+WORKER = "/home/jdboyd/.virtualenvs/pbatch/bin/pbatch_worker"
+stopping = False
+workers = []
+
+def make_worker():
+    pid = os.fork()
+    if pid == 0:
+        print "This is the child"
+        os.execv(WORKER, [WORKER])
+    else:
+        return pid
+
 
 def main(args):
     parser = argparse.ArgumentParser(description='Run jobs int the queue')
@@ -7,6 +25,21 @@ def main(args):
     #parser.add_argument('--single', action="store_true", default=False)
     
     options = parser.parse_args(args)
+
+    while not stopping:
+        if len(workers) != WORKER_CNT:
+            log.info
+            new_worker = make_worker()
+            workers.append(new_worker)
+        else:
+            pass
+
+        for worker in workers:
+            ret = os.waitpid(worker, os.WNOHANG)
+            if ret != (0, 0):
+                workers.remove(worker)
+        time.sleep(5)
+
 
 def start():
     """Entry point.  It just calls main with the CLI args to make it 
